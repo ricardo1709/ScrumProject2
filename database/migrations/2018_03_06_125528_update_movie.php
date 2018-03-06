63e3d6b1466989fc71ddd0156efc603d0205dd20
Unknown <annemiekweterings@outlook.com>
2018-03-06 16:18:32 +0100

    Barcode scanner toegevoegd & controller movies aangepast

---
+++ database/migrations/2018_03_06_125528_update_movie.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateMovie extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->integer('speeltijd');
+            $table->text('genre');  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
