55ecc7524f7fc0aa4507e9685104354ef1022667
Unknown <ricardovbree@gmail.com>
2018-03-08 20:07:16 +0100

    fix migrate bug on linux 2

--- database/migrations/2018_02_23_130524_create_seats.php
+++ database/migrations/2018_02_23_130524_create_seats.php
@@ -13,7 +13,7 @@ class CreateSeats extends Migration
      */
     public function up()
     {
-        Schema::create('Seats', function(Blueprint $table){
+        Schema::create('seats', function(Blueprint $table){
             $table->increments('seatId');
             $table->integer('roomId')->unsigned();
             $table->boolean('isGereserveerd');
