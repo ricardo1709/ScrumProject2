0436173a87b839591a5257fbd6eaf667f51c69c5
Unknown <ro.wemmers@gmail.com>
2018-03-20 13:29:45 +0100

    App linked together and added Admin layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card card-default">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header"><p>Welkom, {{ $currentUser['name'] }}!</p></div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,7 +13,10 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    @if($currentUser['role'] == 3)
+                        <p>Klik hier om naar het Admin Dashboard te gaan,</p>
+                        <a href="/admin" class="btn btn-primary" role="button">Ga naar Admin Dashboard</a>
+                    @endif
                 </div>
             </div>
         </div>
